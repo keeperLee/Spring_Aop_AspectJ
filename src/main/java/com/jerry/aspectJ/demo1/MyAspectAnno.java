ba3ae04c1ed8ba407e6601ee473b8f7123bb883e
keeperlee <leejamesjian@163.com>
2018-10-08 20:22:22 +0800

    AspectJ的注解开发AOP:环绕通知

--- src/main/java/com/jerry/aspectJ/demo1/MyAspectAnno.java
+++ src/main/java/com/jerry/aspectJ/demo1/MyAspectAnno.java
@@ -1,7 +1,9 @@
 package com.jerry.aspectJ.demo1;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
@@ -13,7 +15,6 @@ public class MyAspectAnno {
 
     @Before(value="execution(* com.jerry.aspectJ.demo1.ProductDao.save(..))")
     public void before(JoinPoint joinPoint){
-
         //通过传入一个JoinPoint对象，可以打印出具体的切点信息
         System.out.println("******前置通知*****" + joinPoint);
     }
@@ -22,8 +23,15 @@ public class MyAspectAnno {
     //通过returning属性，可以定义方法返回值，作为参数
     @AfterReturning(value = "execution(* com.jerry.aspectJ.demo1.ProductDao.update(..))",returning = "result")
     public void afterReturing(Object result){
-
         //通过传入一个JoinPoint对象，可以打印出具体的切点信息
         System.out.println("******后置通知*****" + result);
+    }
+
+    @Around(value = "execution(* com.jerry.aspectJ.demo1.ProductDao.delete(..))")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.out.println("环绕前通知+++++++");
+        Object obj = joinPoint.proceed();//执行目标方法
+        System.out.println("环绕后通知+++++++");
+        return obj;
     }
 }
