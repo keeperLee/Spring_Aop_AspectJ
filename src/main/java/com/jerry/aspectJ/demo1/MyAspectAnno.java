2d14e7df887ad65975f15a539096bb4e1ca4a395
keeperlee <leejamesjian@163.com>
2018-10-08 20:31:28 +0800

    AspectJ的注解开发AOP:异常抛出通知

--- src/main/java/com/jerry/aspectJ/demo1/MyAspectAnno.java
+++ src/main/java/com/jerry/aspectJ/demo1/MyAspectAnno.java
@@ -2,10 +2,7 @@ package com.jerry.aspectJ.demo1;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.*;
 
 /*
 切面类
@@ -33,5 +30,11 @@ public class MyAspectAnno {
         Object obj = joinPoint.proceed();//执行目标方法
         System.out.println("环绕后通知+++++++");
         return obj;
+    }
+
+    @AfterThrowing(value = "execution(* com.jerry.aspectJ.demo1.ProductDao.findOne(..))",throwing="e")
+    public void afterThrowing(Throwable e){
+        System.out.println("异常抛出" + e);
+
     }
 }
--- src/main/java/com/jerry/aspectJ/demo1/ProductDao.java
+++ src/main/java/com/jerry/aspectJ/demo1/ProductDao.java
@@ -13,6 +13,7 @@ public class ProductDao {
     }
     public void findOne(){
         System.out.println("查询一个商品");
+        //int i = 1/0;
     }
     public void findAll(){
         System.out.println("查询所有商品");
